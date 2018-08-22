9b1594780201e3fd538f65d95d1bedb502a08564
董宝君 <dongbj@missfresh.cn>
2018-08-22 15:52:24 +0800

    初始提交

---
+++ MFBannerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MFBannerView
+//
+//  Created by 董宝君 on 2018/8/22.
+//  Copyright © 2018年 董宝君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MFBannerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MFBannerView
+//
+//  Created by 董宝君 on 2018/8/22.
+//  Copyright © 2018年 董宝君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
